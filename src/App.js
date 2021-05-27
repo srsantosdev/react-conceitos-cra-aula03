9de05e259cd60d07f9c1e2c89e08bbbcd54d0fe9
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-05-26 23:58:29 -0300

    github-explorer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/global.css'
+import './App.css'
+
+import githubExplorerImage from './assets/logo.svg'
 
 function App() {
+  const [username, setUsername] = useState()
+  const [repositories, setRepositories] = useState(0)
+
+  const [error, setError] = useState(false)
+
+  async function handleSubmit() {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}/repos`)
+      const data = await response.json()
+      
+      setRepositories(data)
+      setError(false)
+    } catch (error) {
+      setError(true)
+    }
+  }
+
+  if(error) {
+    return <p>Error.</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="header-container">
+          <img src={githubExplorerImage} alt="Github Explorer" />
+
+          <div>
+            <input type="text" onChange={event => setUsername(event.target.value)} />
+            <button onClick={handleSubmit}>Pesquisar</button>
+          </div>
+          {error && <p>Usuário não encontrado</p>}
+        </div>
       </header>
+
+      <main>
+        <div className="app-container">
+          <h1>Repositorios de Samuel</h1>
+
+          <ul>
+            {Array.isArray(repositories) ? repositories.map(repository => (
+              <li className="repository-item">
+                <div>
+                  <strong>{repository.name}</strong>
+                  <span>{repository.language}</span>
+                </div>
+
+                <a href={repository.html_url}>
+                  Acessar repositório
+                </a>
+              </li>
+            )) : (
+              <p>Usuário não foi encontrado.</p>
+            )}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
